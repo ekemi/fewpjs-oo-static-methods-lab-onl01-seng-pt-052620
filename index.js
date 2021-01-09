8e47a78f592e7b119b6d78e7d7374a953948fa82
edmundo papi <edmundopierre@yahoo.com>
2021-01-09 03:30:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,29 @@ class Formatter {
   static sanitize(st){
     return st.replace(/[^A-Za-z0-9-'\s]/g,'')
   }
+
+  static titleize(st){
+    for ( let i =0; i<y;i++){
+var p = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from']
+
+var x = "where the wild things are"
+x = x.split(' ')
+
+var y = x.lenth
+var c =0
+var temp = []
+
+
+x = x.split(' ')
+
+if (x[i]===p[i]){
+    temp.push(x[i])
+    console.log(temp)
+}
+else if( x[i]!==y[i]) {
+temp.push(x[i][0].toUpperCase+x[i].slice(1,))
+console.log(temp)
+}
+    
+  }
 }
